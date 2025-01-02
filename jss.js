d7a344c1865ec40e2d01bec6573c803683b377a2
ell rawrrr <hikmahtul.devi@gmail.com>
2025-01-02 14:12:03 +0700

    Delete jss.js

--- jss.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('wishButton').addEventListener('click', function() {
-    const wishMessage = document.getElementById('wishMessage');
-    wishMessage.textContent = "Semoga semua impianmu tercapai dan selalu bahagia! Selamat ulang tahun, Prashila!";
-    wishMessage.classList.remove('hidden');
-});
